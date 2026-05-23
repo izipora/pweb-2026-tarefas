f6488cd383f195f73645794e3244fd6e3d4cf0f4
camily <camilyzipora08@gmail.com>
2026-05-23 11:41:39 -0300

    tarefa 03

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def usuarios(request):
+
+    lista_usuarios = [
+        {"nome": "Rubens", "matricula": "1464", "idade": 25},
+        {"nome": "Erizebete", "matricula": "1357", "idade": 20},
+        {"nome": "José da Silva Sauro", "matricula": "1235", "idade": 42},
+        {"nome": "Sinvastantino ", "matricula": "2314", "idade": 57},
+        {"nome": "Luigi", "matricula": "3197", "idade": 31}
+   
+    ]
+
+    context = {
+        "usuarios": lista_usuarios
+    }
+
+    return render(request, "usuarios.html", context)
