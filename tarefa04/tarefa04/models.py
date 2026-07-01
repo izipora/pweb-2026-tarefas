bdc7bf3944680ea721f15d59d6796d4b3f686173
camily <camilyzipora08@gmail.com>
2026-07-01 16:06:41 -0300

    criando classe  Missão

--- tarefa04/tarefa04/models.py
+++ tarefa04/tarefa04/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Missão (models.Model):
+    nome = models.CharField(max_length=200)
+    status = models.BooleanField()
+    prazo = models.DateField()
+
+    def __str__(self):
+         if self.status:
+            status = "Missão concluída 😊💯"
+         else: 
+            "Missão pendente 🥲❌"
+         return f"Nome: {self.nome} | Status: {status} | Prazo: {self.prazo}"
